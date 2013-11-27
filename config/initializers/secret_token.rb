61b1fbce86fbba7fcb57b0a9422561db85fc435d
Jeffrey Leung <jeffreyleung@alumni.calpoly.edu>
2013-11-26 19:55:26 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Alerthub::Application.config.secret_key_base = 'cfae05040b7469416b4dc5c976f1aa2d752c3ca495aaa7fe8c42d5ff252296deaa5f148988766f49dc68e947bf9b4aecb2f6630d09c6bf807be6da3d9fd82c96'
